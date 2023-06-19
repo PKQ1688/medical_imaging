de38410d1f727494d44c957e2c1ad9b2176d393e
zhutaonan <adolf1321794021@gmail.com>
2023-06-19 19:36:30 +0800

    update

---
+++ CT-DL-AGC-Recurrence/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time         : 2023/5/20 22:11
+# @Author       : ztn8btc
+# @Email        : zhutn@8btc.com
+# @LastEditTime : 2023/5/20 22:11
+# @File         : __init__.py.py
---
+++ CT-DL-AGC-Recurrence/data_show.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time         : 2023/5/20 22:31
+# @Author       : ztn8btc
+# @Email        : zhutn@8btc.com
+# @LastEditTime : 2023/5/20 22:31
+# @File         : data_show.py
+import SimpleITK as sitk
+import numpy as np
+import cv2
+import os
+
+path = "data/0517/原序列/1V/"
+#
+reader = sitk.ImageSeriesReader()
+dicom_names = reader.GetGDCMSeriesFileNames(path)
+reader.SetFileNames(dicom_names)
+image = reader.Execute()
+image_array = sitk.GetArrayFromImage(image)     # (z,y,x): z:切片数量,y:切片宽,x:切片高
+
+print(image_array.shape)
+print(image_array)
+# label_file = "data/0517/勾画/1V_Merge.nii.gz"
+
+
+# def read_img(path):
+#     img = sitk.ReadImage(path)
+#     data = sitk.GetArrayFromImage(img)
+#     print(data.shape)
+#     print(data)
+#     return data
+#
+#
+# read_img(label_file)
