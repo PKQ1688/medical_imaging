a8bbf491a88e20649b8e3062521c00d53259e475
adolf <adolf1321794021@gmail.com>
2024-01-23 11:44:23 +0800

    update

--- monai_use/auto_seg.py
+++ monai_use/auto_seg.py
@@ -1,10 +1,30 @@
-import yaml
-from monai.apps.auto3dseg import AutoRunner
+import os
+import shutil
 
-with open('monai_use/input.yaml', 'r') as file:
-    data = yaml.safe_load(file)
+roi_data = "data/Task100/roi"
 
-# 打印读取的数据
-print(data)
+ori_dcom = "data/ori_dcm"
+dcom_list = os.listdir(ori_dcom)
 
-AutoRunner(input="monai_use/input.yaml").run()
+
+# print(dcom_list)
+
+def copy_folder(src, dst):
+    """
+    复制src目录到dst目录
+    :param src: 源目录
+    :param dst: 目标目录
+    """
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    shutil.copytree(src, dst, dirs_exist_ok=True)
+
+
+for file in os.listdir(roi_data):
+    file_dcom = file.split("_")[0]
+    # prinfilet(file)
+    if file_dcom in dcom_list:
+        copy_folder(src=os.path.join(ori_dcom, file_dcom),
+                    dst=os.path.join("data/Task100/ori_dcm", file_dcom))
+    else:
+        print(file_dcom)
