9eb94e30ef9bab67ab22547dc8b5735cf559adf7
adolf <adolf1321794021@gmail.com>
2024-06-10 19:13:58 +0800

    update

--- use_huggingface/inference.py
+++ use_huggingface/inference.py
@@ -1,87 +1,29 @@
 import torch
 import pandas as pd
-from transformers import SegformerImageProcessor, SegformerForSemanticSegmentation
 from PIL import Image
 import os
 import torch.nn.functional as F
-from tqdm import tqdm
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from transformers import MaskFormerFeatureExtractor,MaskFormerForInstanceSegmentation
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_name = "segformer-b0-finetuned-segments-sidewalk-outputs/checkpoint-7240"
-feature_extractor = SegformerImageProcessor.from_pretrained(model_name)
-model = SegformerForSemanticSegmentation.from_pretrained(model_name).to(device)
+model_name = "use_huggingface/final_checkpoint"
+
+feature_extractor = MaskFormerFeatureExtractor.from_pretrained(model_name)
+# inputs = feature_extractor(images=image, return_tensors="pt")
+model = MaskFormerForInstanceSegmentation.from_pretrained(model_name).to(device)
 
 label_df = pd.read_csv("test_label.csv")
-# img_path = 'res_data/origin/1V_28.png'
-def get_pred(img_paths):
-    images = [Image.open(img_path) for img_path in img_paths]
-    inputs = feature_extractor(images=images, return_tensors="pt").to(device)
+
+_image_name = "9v"  
+img_paths = os.listdir(os.path.join("test_data_origin",_image_name))
+img_paths = [os.path.join("test_data_origin",_image_name,img_name) for img_name in img_paths]
+images = [Image.open(img_path) for img_path in img_paths]
+print(len(images[0].getbands()))
+
+
+for image in images:
+    inputs = feature_extractor(images=image, return_tensors="pt").to(device)
     outputs = model(**inputs)
-    logits = outputs.logits
+    pred_segs = feature_extractor.post_process_semantic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
 
-    upsampled_logits = F.interpolate(
-        logits,
-        size=images[0].size[::-1],  # Assuming all images are of the same size
-        mode='bilinear',
-        align_corners=False
-    )
-
-    pred_segs = upsampled_logits.argmax(dim=1)
-
-    results = []
-    for pred_seg in pred_segs:
-        flat_tensor = pred_seg.view(-1)
-        
-        # Count the occurrences of each class
-        unique, counts = torch.unique(flat_tensor, return_counts=True)
-        counts_dict = dict(zip(unique.cpu().numpy(), counts.cpu().numpy()))
-        
-        # Get the counts of classes 0, 1, and 2
-        zeros = counts_dict.get(0, 0)
-        ones = counts_dict.get(1, 0)
-        twos = counts_dict.get(2, 0)
-        
-        # Determine the result based on counts of classes 1 and 2
-        if ones > twos:
-            results.append(0)
-        else:
-            results.append(1)
-    
-    return results
-
-img_list = os.listdir('res_data/origin')
-img_paths = [os.path.join('res_data/origin', img) for img in img_list]
-print(f"Total images: {len(img_paths)}")
-
-# Example of processing in batches
-# batch_size = 16
-predictions = []
-labels = []
-for i in tqdm(range(0, len(img_paths), batch_size),total=int(len(img_paths)//batch_size)):
-    batch_paths = img_paths[i:i+batch_size]
-    # import pdb
-    # pdb.set_trace()
-    batch_labels = []
-    for image_name in batch_paths:
-        img_name = os.path.basename(image_name)
-        img_index = int(img_name.split(".")[0].split("_")[0].replace("V", ""))
-        category_name = int(label_df[label_df["number"] == img_index]["label"].values[0])
-        batch_labels.append(category_name)
-    predictions.extend(get_pred(batch_paths))
-    labels.extend(batch_labels)
-
-print(predictions)
-print(labels)
-
-# 计算各项指标
-accuracy = accuracy_score(labels, predictions)
-precision = precision_score(labels, predictions)
-recall = recall_score(labels, predictions)
-f1 = f1_score(labels, predictions)
-
-# 打印结果
-print(f"Accuracy: {accuracy}")
-print(f"Precision: {precision}")
-print(f"Recall: {recall}")
-print(f"F1 Score: {f1}")
+print(pred_segs.shape)
